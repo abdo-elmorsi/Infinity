a6c73b1abb0cbe5165d76b6ef7a2f54af248f668
Abdo2711-coder <abdelrahmandiv@gmail.com>
2022-11-08 00:12:25 +0200

    translate

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -1,9 +1,14 @@
+import Cookies from "js-cookie";
+import { useTranslation } from "react-i18next"
+
 export const Features = (props) => {
+  const { t } = useTranslation();
+  const lang = Cookies.get("i18next") || "en";
   return (
     <div id='features' className='text-center'>
       <div className='container'>
         <div className='section-title'>
-          <h2>Features</h2>
+          <h2>{t("features_key")}</h2>
         </div>
         <div className='row'>
           {props.data
@@ -14,11 +19,11 @@ export const Features = (props) => {
                     <img src={d.img} alt={d.title} />
                   </div>
                   <div className={`col-lg-6 ${i % 2 == 1 ? "order-1" : "order-2"}`}>
-                    <h2>{d.title}</h2>
+                    <h2>{t(d.title)}</h2>
                     <ul className="list-unstyled mt-3 mb-4">
                       {d.options.map((c, b) => {
                         return (
-                          <li className="text-left mb-4">{b + 1}: {c}</li>
+                          <li className={`${lang == "en" ? "text-left" : "text-right"} mb-4`}>{b + 1}: {t(c)}</li>
                         )
                       })}
 
