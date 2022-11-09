08a6fcf310a8b5322e33ecb19c9bd4e6f0211daf
Abdo2711-coder <abdelrahmandiv@gmail.com>
2022-11-09 06:24:43 +0200

    .

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import HttpApi from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import Loader from './components/loader';
 
+import bootstrap from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const App = React.lazy(() => import('./App'));
 
